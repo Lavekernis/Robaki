bdc95a40a639d95557514d262f722dea6f49a2cf
Wojciech Walaszek <wwalaszek02@gmail.com>
2020-05-02 20:27:31 +0200

    debugging

--- game.py
+++ game.py
@@ -1,7 +1,12 @@
 import variables as var
+from pygame.math import Vector2
 from map import *
 
 map = Map("Map.bmp")
+
+pygame.font.init()
+font = font = pygame.font.SysFont("comicsansms", 48)
+
 
 def cameraScrolling():
     if var.scroling_X_Inc and var.camera_vector.x + var.SCREEN_WITH < map.surface.get_width():
@@ -11,8 +16,29 @@ def cameraScrolling():
     if var.scroling_Y_Inc and var.camera_vector.y + var.SCREEN_HEIGHT < map.surface.get_height():
         var.camera_vector.y += 10
     if var.scroling_Y_Dec and var.camera_vector.y > 0:
-        var.camera_vector.y -= 10    
+        var.camera_vector.y -= 10
+
 
 def update(screen):
     cameraScrolling()
     map.draw(screen)
+
+    world_x = var.camera_vector.y + (var.SCREEN_WITH/2)
+    world_y = var.camera_vector.x + (var.SCREEN_HEIGHT / 2)
+    screen_center = (round(var.SCREEN_WITH/2), round(var.SCREEN_HEIGHT / 2))
+    radius = 50
+
+    text = font.render("X: " + str(round(world_x)) + " Y:" +
+                       str(round(world_y)), True, (0, 128, 128))
+
+    pygame.draw.circle(screen, (255, 0, 0), screen_center, radius)
+
+    info = map.circle_collision(Vector2(world_x, world_y), radius)
+    if info != None:
+        text2 = font.render("Normal: (" + str(round(info.normal.x)) +
+                            ", " + str(round(info.normal.y)) + ")", True, (0, 128, 128))
+        pygame.draw.line(screen, (0, 255, 0), screen_center,
+                         screen_center + (info.normal * radius), 5)
+        screen.blit(text2, (0, 48 + 5))
+
+    screen.blit(text, (0, 0))
--- map.py
+++ map.py
@@ -1,8 +1,9 @@
 import variables as var
 import os
-from collision_info import CollisionInfo
-import pygame.math
 import pygame
+from collision_info import CollisionInfo
+from pygame.math import Vector2
+from math import sin, cos
 
 
 class Map():
@@ -16,21 +17,27 @@ class Map():
         screen.blit(self.surface, (0, 0), camera_image)
 
     def circle_collision(self, pos, radius, points=36):
-        """Checks intersection of circle given the radius and position. Creates n amount of points on the circle to calculate proper normal."""
+        """Checks intersection of circle given the radius and position.
+        Creates n amount of points on the circle to calculate proper normal.
+        Returns CollisionInfo if collided, None otherwise."""
         phi = 360.0 / points
         intersects = []
 
         for i in range(0, points):
-            point_coordinate = (round(sin(phi * i)) * radius,
-                                round(cos(phi * i)) * radius)
-            point_coordinate += Vector2(round(pos.x), round(pos.y))
+            point_coordinate = Vector2(sin(phi * i),
+                                       cos(phi * i)) * radius
+            point_coordinate += Vector2(pos.x, pos.y)
+
+            rx = round(point_coordinate.x)
+            ry = round(point_coordinate.y)
 
             # out of bounds check
-            if point_coordinate.x >= self.surface.get_width() or point_coordinate.x < 0 or point_coordinate.y >= self.surface.get_height() or point_coordinate.y < 0:
+            if rx >= self.surface.get_width() or rx < 0 or ry >= self.surface.get_height() or ry < 0:
                 continue
 
             # (0,0,0) is black colour, currently used to symbolize terrain
-            if self.surface.get_at(point_coordinate) == (0, 0, 0):
+            if self.surface.get_at((rx, ry)) == (0, 0, 0):
+                print("intersect happened at: " + str(point_coordinate))
                 intersects.append(point_coordinate)
 
         if len(intersects) == 0:
@@ -41,4 +48,3 @@ class Map():
                 normal -= item
             normal = normal.normalize()
             return CollisionInfo(normal)
-        self.surface = pygame.image.load(var.os.path.join(var.filepath, path))
