8df995c2d66793cc8a29ec211b7f9ab5236729c7
Lavekernis <lavekernis@gmail.com>
2020-05-02 16:31:08 +0200

    Repaired main

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
 
 pygame.init()
-clock = pygame.time.Clock()
 
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((800,600))
 running = True
 
 while running:
+
+    screen.fill((0,0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
+
+    pygame.display.update()
     clock.tick(60)
